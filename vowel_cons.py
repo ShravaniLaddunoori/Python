992631ff5ee3b6ef033aae164541e3e78b59fad3
Shravani <sravaniladdunoori1637@gmail.com>
2021-11-26 16:31:44 +0530

    Vowel or Consonant

---
+++ vowel_cons.py
@@ -0,0 +1,6 @@
+input_str = input('Enter a character:')
+if ((input_str == 'a' or input_str == 'e' or input_str == 'i' or input_str == 'o' or input_str == 'u') or
+(input_str == 'A' or input_str == 'E' or input_str == 'I' or input_str == 'O' or input_str == 'U')):
+    print('vowel')
+else:
+    print('consonant')
